2a1043942ccceb47e39b072cb95e4ee87a83a416
Aishwarya <aishwarya.asah@gmail.com>
2020-02-01 01:48:12 +0530

    Support page

--- 4-walls-for-all/client/src/component/supportus.js
+++ 4-walls-for-all/client/src/component/supportus.js
@@ -1,12 +1,35 @@
 import React from 'react'
-
+import "./supportus.css";
+import support from "../media/images/support.png";
+import donate from "../media/images/donate.png";
 
 class SupportUs extends React.Component {
   
   render() {
     return (
       <div className="supportus">
-        SUPPORT US
+        <div className="banner">
+          <p>Support Us</p>
+        </div>
+        <div className="infotext">
+          <h2>Get Involved</h2>
+          <p>
+          We will be really honoured to have you on-board with us. We are open to both 
+          monetary and non-monetary forms of contributions. Check our Events details to find more.
+          </p>
+          <br></br>
+          <button>Contributors</button>
+        </div>
+        <div className="imgArea support">
+          <img src={support}></img>
+        </div>
+        <div className="infotext">
+          <h2>Let's Make A Change</h2>
+          <p>Here are some ways you can donate:</p>
+        </div>
+        <div className="imgArea donate">
+          <img src={donate}></img>
+        </div>
       </div>
     );
   }
